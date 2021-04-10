c00e397684b22ba2b02099f4886540c2fb38761f
maks6169 <maks6169@rambler.ru>
2021-04-10 10:39:40 +0300

    Initial commit

---
+++ src/geekbrains/lesson_8v2/XOStarter.java
@@ -0,0 +1,14 @@
+package geekbrains.lesson_8v2;
+
+/**
+ * Project java_core_1_0321
+ *
+ * @Author Alexander Grigorev
+ * Created 03.04.2021
+ * v1.0
+ */
+public class XOStarter {
+    public static void main(String[] args) {
+        new GameWindow();
+    }
+}
